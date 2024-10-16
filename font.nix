b5544c1fa987b653e2a2b27b06e010e9599e4590
Samuel Nilsson <samuel@samuelnilsson.net>
2024-10-16 21:58:31 +0200

    import fonts correctly

--- font.nix
+++ font.nix
@@ -1,14 +1,15 @@
 self: { config, lib, pkgs, ... }:
-with pkgs; [
-  (nerdfonts.override { fonts = [ "Iosevka" ]; })
-  (iosevka.override {
-    privateBuildPlan = {
-      family = "Iosevka Aile";
-      spacing = "quasi-proportional";
-      serifs = "sans";
-      noCvSs = true;
-      exportGlyphNames = false;
-    };
-    set = "Aile";
-  })
-]
+{
+  fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "Iosevka" ]; })
+    (iosevka.override {
+      privateBuildPlan = {
+        family = "Iosevka Aile";
+        spacing = "quasi-proportional";
+        serifs = "sans";
+        noCvSs = true;
+        exportGlyphNames = false;
+      };
+      set = "Aile";
+    })
+  ]}
