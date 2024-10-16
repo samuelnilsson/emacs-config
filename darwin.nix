0cc0df3a9666296dc7a228250cf7594d64a4adee
Samuel Nilsson <samuel@samuelnilsson.net>
2024-10-16 22:08:19 +0200

    remove font module

--- darwin.nix
+++ darwin.nix
@@ -17,6 +17,18 @@ in
       package = emacs;
     };
 
-    fonts.packages = (import ./font.nix self).fonts;
+    fonts.packages = with pkgs; [
+      (nerdfonts.override { fonts = [ "Iosevka" ]; })
+      (iosevka.override {
+        privateBuildPlan = {
+          family = "Iosevka Aile";
+          spacing = "quasi-proportional";
+          serifs = "sans";
+          noCvSs = true;
+          exportGlyphNames = false;
+        };
+        set = "Aile";
+      })
+    ];
   };
 }
