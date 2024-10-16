c241f2aba5513d2960ddbfcb50f7eca86bca3392
Samuel Nilsson <samuel@samuelnilsson.net>
2024-10-16 22:19:41 +0200

    reduce code duplication

---
+++ font.nix
@@ -0,0 +1,13 @@
+pkgs: with pkgs; [
+  (nerdfonts.override { fonts = [ "Iosevka" ]; })
+  (iosevka.override {
+    privateBuildPlan = {
+      family = "Iosevka Aile";
+      spacing = "quasi-proportional";
+      serifs = "sans";
+      noCvSs = true;
+      exportGlyphNames = false;
+    };
+    set = "Aile";
+  })
+]
