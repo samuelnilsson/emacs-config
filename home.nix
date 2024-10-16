429beace0002fea635dc59d3f298ca347ff438c9
Samuel Nilsson <samuel@samuelnilsson.net>
2024-10-16 22:08:19 +0200

    remove font module

--- home.nix
+++ home.nix
@@ -23,6 +23,18 @@ in
       package = emacs;
     };
 
-    home.packages = (import ./font.nix).fonts;
+    home.packages = with pkgs; [
+      (nerdfonts.override { fonts = [ "Iosevka" ]; })
+      (iosevka.override {
+        privateBuildPlan = {
+          family = "Iosevka Aile";
+          spacing = "quasi-proportional";
+          serifs = "sans";
+          noCvSs = true;
+          exportGlyphNames = false;
+        };
+        set = "Aile";
+      })
+    ];
   };
 }
